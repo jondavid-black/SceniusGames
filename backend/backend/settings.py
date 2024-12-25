cf608fd5c4dd143aa32b1cb2a70aa84a6678d987
JD Black <black.jondavid@gmail.com>
2024-12-25 08:32:53 -0600

    added apps to backend, updated readme

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'scenius',
+    'planningpoker',
+    'leancoffee',
+    'dotvoting',
 ]
 
 MIDDLEWARE = [
